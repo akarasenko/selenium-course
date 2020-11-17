fd8782659e93ef9375622016a3ff37192108e3ea
akarasenko <asya2794@mail.ru>
2020-11-17 21:09:16 +0300

    ÐÐ°Ð´Ð°Ð½Ð¸Ðµ 3 Ð¡Ð´ÐµÐ»Ð°Ð¹ÑÐµ ÑÑÐµÐ½Ð°ÑÐ¸Ð¹ ÐÐ»Ð¾Ð³Ð¸Ð½Ð° done

--- src/test/java/ru/stqa/example/myFirstTest.java
+++ src/test/java/ru/stqa/example/myFirstTest.java
@@ -3,6 +3,7 @@ package ru.stqa.example;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -16,8 +17,11 @@ public class myFirstTest{
     }
 
     @Test
-    public void myFirstTest() {
-        driver.navigate().to("http://www.google.com");
+    public void myFirstTest1() {
+        driver.navigate().to("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("username")).sendKeys("password");
+        driver.findElement(By.name("login")).click();
      }
 
     @After
