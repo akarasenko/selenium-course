75740e9b89bb847f9a7e87d26cd9aa622f2a4a69
akarasenko <asya2794@mail.ru>
2020-11-28 23:50:42 +0300

    Task 2. Check country sort in admin panel done

--- src/test/java/ru/stqa/example/Task9Test.java
+++ src/test/java/ru/stqa/example/Task9Test.java
@@ -26,22 +26,24 @@ public class Task9Test {
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
         System.out.println(((HasCapabilities)driver).getCapabilities());
+
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("123qwe");
+        driver.findElement(By.name("login")).click();
     }
 
     @Test
-    public void task9Test() {
+    public void task9Part1() {
 
         var baseUrl = "http://localhost/litecart/admin/?app=countries&doc=countries";
 
         driver.navigate().to(baseUrl);
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("123qwe");
-        driver.findElement(By.name("login")).click();
 
-        var contryList = driver.findElements(By.xpath("//form[@name='countries_form']//*[@class='row']//td[5]"))
+        var countryList = driver.findElements(By.xpath("//form[@name='countries_form']//*[@class='row']//td[5]"))
                 .stream().map(element -> element.getText()).collect(Collectors.toList());
 
-        Assert.assertTrue(Ordering.natural().isOrdered(contryList));
+        Assert.assertTrue(Ordering.natural().isOrdered(countryList));
 
         var zoneCountList = driver.findElements(By.xpath("//form[@name='countries_form']//*[@class='row']//td[6]"));
         var countryToCheckZones = new ArrayList<String>();
@@ -60,6 +62,25 @@ public class Task9Test {
             var zoneList = driver.findElements(By.xpath("//table[@id='table-zones']//td[3]//input"))
                     .stream().map(element -> element.getAttribute("value")).filter(text -> text != "").collect(Collectors.toList());
             Assert.assertTrue(Ordering.natural().isOrdered(zoneList));
+
+            driver.navigate().to(baseUrl);
+        }
+    }
+
+    @Test
+    public void task9Part2() {
+        var baseUrl = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
+
+        driver.navigate().to(baseUrl);
+        var zonesToCheck = driver.findElements(By.xpath("//form[@name='geo_zones_form']//tr[@class='row']//td[3]//a"))
+                .stream().map(element -> element.getAttribute("href")).collect(Collectors.toList());
+
+        for (var zone :zonesToCheck) {
+            driver.navigate().to(zone);
+            var zones = driver.findElements(By.xpath("//table[@id='table-zones']//tr//td[3]//option[@selected]"))
+                    .stream().map(element -> element.getText()).collect(Collectors.toList());
+
+            Assert.assertTrue(Ordering.natural().isOrdered(zones));
 
             driver.navigate().to(baseUrl);
         }
