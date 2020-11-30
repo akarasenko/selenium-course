af6c7c30e2f3f25c6ce010466135e88e064845c7
akarasenko <asya2794@mail.ru>
2020-11-30 10:58:07 +0300

    Task 11 Random part to email added / minor fix

--- src/test/java/ru/stqa/example/Task11Test.java
+++ src/test/java/ru/stqa/example/Task11Test.java
@@ -25,13 +25,15 @@ public class Task11Test {
     public void task7Test() {
 
         var randomPart = Long.toString(Math.round(Math.random() * 100000));
+        var firstName = "TestFirstName" + randomPart;
+        var email = "test" + randomPart + "@test.com";
 
         driver.navigate().to("http://localhost/litecart/en/");
         driver.findElement(By.xpath("//form[@name='login_form']//a[text()='New customers click here']")).click();
 
         driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='tax_id']")).sendKeys("TestTaxId");
         driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='company']")).sendKeys("TestCompany");
-        driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='firstname']")).sendKeys("TestFirstName" + randomPart);
+        driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='firstname']")).sendKeys(firstName);
         driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='lastname']")).sendKeys("TestLastName");
         driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='address1']")).sendKeys("TestAddress1");
         driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='address2']")).sendKeys("TestAddress2");
@@ -41,7 +43,7 @@ public class Task11Test {
         var countryCodeSelect = new Select(driver.findElement(By.xpath("//select[@name='country_code']")));
         countryCodeSelect.selectByVisibleText("United States");
 
-        driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='email']")).sendKeys("test" + randomPart + "@test.com");
+        driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='email']")).sendKeys(email);
         driver.findElement(By.xpath("//form[@name='customer_form']//input[@name='phone']")).sendKeys("+71112224455");
 
         selectCheckbox(driver,"//form[@name='customer_form']//input[@type='checkbox']");
@@ -53,7 +55,7 @@ public class Task11Test {
 
         driver.findElement(By.xpath("//div[@id='box-account']//a[text()='Logout']")).click();
 
-        driver.findElement(By.xpath("//div[@id='box-account-login']//input[@name='email']")).sendKeys("test" + randomPart + "@test.com");
+        driver.findElement(By.xpath("//div[@id='box-account-login']//input[@name='email']")).sendKeys(email);
         driver.findElement(By.xpath("//div[@id='box-account-login']//input[@name='password']")).sendKeys("123qwe");
         driver.findElement(By.xpath("//div[@id='box-account-login']//button[@name='login']")).click();
 
