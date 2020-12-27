8aedcb2f0453d95bcce579c68468839a04e2293d
ruaqdbn <anastasia.karasenko@raiffeisen.ru>
2020-12-27 13:14:00 +0700

    Task 20. Add parametr to adding product to cart method and reformat scenario

--- src/test/java/ru/stqa/cucumber/MySteps.java
+++ src/test/java/ru/stqa/cucumber/MySteps.java
@@ -2,6 +2,8 @@ package ru.stqa.cucumber;
 
 import io.cucumber.java.bs.A;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import ru.stqa.example.application.Application;
 
@@ -9,23 +11,30 @@ import ru.stqa.example.application.Application;
 public class MySteps {
     public static Application app = new Application();
 
-    @When("go to main page")
+    @Given("go to main page")
     public void goToMainPage() {
         app.gotoMainPage();
     }
 
-    @And("add random product to cart")
-    public void addRandomProductToCart() {
-        app.addRandomProductToCart();
+    @And("add {string} random products to cart")
+    public void addRandomProductsToCart(int productCount) {
+        for (int i = 0; i < productCount; i++) {
+            app.addRandomProductToCart();
+            app.gotoMainPage();
+        }
     }
 
-    @When("go to cart")
+    @Then("go to cart")
     public void goToCart() {
         app.goToCart();
     }
 
-    @And("delete random product from cart")
-    public void deleteRandomProductFromCart() {
-        app.deleteRandomProductFromCart();
+    @And("delete all products from cart")
+    public void deleteAllProductsFromCart() {
+        var productAmount = app.getProductAmountInCart();
+
+        for (int i = 0; i < productAmount; i++) {
+            app.deleteRandomProductFromCart();
+        }
     }
 }
--- src/test/resources/ru.stqa.cucumber/Task20Test.feature
+++ src/test/resources/ru.stqa.cucumber/Task20Test.feature
@@ -1,11 +1,8 @@
 #language:en
 
 Feature: Add products to cart and delete them
-  Scenario: : Add products to cart
-    When go to main page
-    And add random product to cart
-    And go to main page
-
-  Scenario: Delete product from cart
-    When go to cart
-    And delete random product from cart
+  Scenario: : Add products to cart and delete them
+    Given go to main page
+    When add "3" random products to cart
+    Then go to cart
+    And delete all products from cart
